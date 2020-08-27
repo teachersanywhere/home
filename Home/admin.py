be830d79cb83ae9733bdbe053c525650befcc7c4
ayushb2002 <ayushb6732@gmail.com>
2020-08-27 14:21:48 +0530

    second commit

--- Home/admin.py
+++ Home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Location, Teacher
+from .models import Location, Teacher, Subject
 
 admin.site.register(Location)
 admin.site.register(Teacher)
+admin.site.register(Subject)
 
--- Home/views.py
+++ Home/views.py
@@ -1,25 +1,36 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from .models import Location, Teacher
+from .models import Location, Teacher, Subject
 # Create your views here.
 def index(request):
     return render(request, "home/index.html")
 
 def findTeacher(request):
     context = {
-        "location": Location.objects.all()
+        "location": Location.objects.all(),
+        "subjects": Subject.objects.all() 
     }
     return render(request, "home/findTeacher.html", context)
 
 def showTeacher(request):
     area = request.POST['pincode']
+    classes = request.POST['class-filter']
+    subject = request.POST['subject-filter']
+    sb = Subject.objects.get(subject=subject)
     location = Location.objects.get(area=area)
     teachers = []
+    final = []
     for obj in Teacher.objects.filter(location=location):
         teachers.append(obj)
 
+    for t in teachers:
+        if t.classes == classes:
+            if t.subject1 == sb or t.subject2 == sb or t.subject3 == sb:
+                final.append(t)
+                
+
     context = {
-        'teachers': teachers 
+        'teachers': final
     }
 
     return render(request, "home/showTeacher.html", context)
